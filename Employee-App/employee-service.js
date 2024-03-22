fe26ae33196476c471dbe6e907e0b0ab8b6808ce
MinhPhamLapTrinh <116688660+MinhPhamLapTrinh@users.noreply.github.com>
2024-03-21 20:02:27 -0400

    update functionality

---
+++ Employee-App/employee-service.js
@@ -0,0 +1,146 @@
+import mongoose from "mongoose";
+import env from "dotenv";
+env.config();
+
+let mongoDBConnectionString = process.env.MONGO_URI;
+
+let Schema = mongoose.Schema;
+
+let timeRecordSchema = new Schema({
+  date: Date,
+  startTime: Date,
+  endTime: Date,
+  totalWorkingHours: Number,
+});
+
+let employeeSchema = new Schema({
+  employeeName: String,
+  uniqueNum: String,
+  timeRecord: [timeRecordSchema],
+});
+
+let Employee;
+
+export function connect() {
+  return new Promise(function (resolve, reject) {
+    let db = mongoose.createConnection(mongoDBConnectionString);
+
+    db.on("error", (err) => {
+      reject(err);
+    });
+
+    db.once("open", () => {
+      Employee = db.model("Employee", employeeSchema);
+      resolve();
+    });
+  });
+}
+
+export function addEmployee(name, uniqueNum) {
+  return new Promise(function (resolve, reject) {
+    const newEmployee = new Employee({
+      employeeName: name,
+      uniqueNum: uniqueNum,
+      // timeRecord: [timeRecordSchema],
+    });
+
+    newEmployee
+      .save()
+      .then(() => {
+        resolve("Employee " + newEmployee.employeeName + " successfully added");
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+export function checkInEmployee(uniqueNum) {
+  return new Promise(function (resolve, reject) {
+    Employee.findOne({ uniqueNum: uniqueNum })
+      .exec()
+      .then((emp) => {
+        resolve(emp);
+      })
+      .catch((err) => {
+        reject("invalid pin " + err);
+      });
+  });
+}
+
+export function employeeClockIn(id) {
+  return new Promise(function (resolve, reject) {
+    Employee.findById(id)
+      .exec()
+      .then((emp) => {
+        const today = new Date().setHours(0, 0, 0, 0);
+        const lastRecord = emp.timeRecord[emp.timeRecord.length - 1];
+        if (
+          lastRecord &&
+          new Date(lastRecord.date).setHours(0, 0, 0, 0) === today
+        ) {
+          reject("You have already clocked in today");
+        } else {
+          const startTime = new Date().toLocaleString();
+          emp.timeRecord.push({
+            date: startTime,
+            startTime: startTime,
+            endTime: null,
+            totalWorkingHours: 0,
+          });
+          console.log(emp);
+          emp
+            .save()
+            .then(() => {
+              resolve(`you've been successfully clocked in at  ${startTime}`);
+            })
+            .catch((err) => {
+              reject(`Cannot clock in ` + err);
+            });
+        }
+      });
+  });
+}
+
+export function employeeClockOut(id) {
+  return new Promise(function (resolve, reject) {
+    Employee.findById(id)
+      .exec()
+      .then((emp) => {
+        const today = new Date().setHours(0, 0, 0, 0);
+        const timeRecord = emp.timeRecord[emp.timeRecord.length - 1];
+        if (
+          !timeRecord ||
+          new Date(timeRecord.startTime).setHours(0, 0, 0, 0) !== today
+        ) {
+          reject("You have to clock in first!");
+        } else {
+          const endTime = new Date().toLocaleString();
+          const start = new Date(timeRecord.startTime).getHours();
+          console.log(start);
+          const end = new Date(timeRecord.endTime).getHours();
+          const totalWorkingHours = end - start;
+          Employee.updateOne(
+            {
+              _id: id,
+              "timeRecord._id": timeRecord._id,
+            },
+            {
+              $set: {
+                "timeRecord.$.endTime": endTime,
+                "timeRecord.$.totalWorkingHours": totalWorkingHours,
+              },
+            }
+          )
+            .then(() => {
+              resolve(
+                `you've been successfully clocked out at  ${endTime}. Total working hours: ${totalWorkingHours} hours.`
+              );
+            })
+            .catch((err) => {
+              reject(`Cannot clock out due to ${err}`);
+            });
+        }
+      });
+  });
+}
--- Employee-App/server.js
+++ Employee-App/server.js
@@ -1,23 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import env from "dotenv";
+env.config();
+
+import * as employeeService from "./employee-service.js";
 
 const app = express();
 const PORT = 4000; // Port number  to run the server
-
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Employee Object
-
-let employee = [
-  {
-    id: 1,
-    name: "Duc Minh",
-    uniqueNum: "112",
-    timeRecord: [],
-  },
-];
 
 app.get("/", (req, res) => {
   res.send("Welcome to Employee App");
@@ -26,80 +18,88 @@ app.get("/", (req, res) => {
 app.post("/check-in", (req, res) => {
   const pin = req.body.pin; // get the pin from the request body
   const selection = req.body.selection;
-  console.log(typeof pin);
-  const result = employee.find((emp) => parseInt(emp.uniqueNum) === pin); // find the employee with the given pin
-  console.log(result);
-  if (result) {
-    if (selection === "clock-in") {
-      res.redirect(`/clock-in/${pin}`);
-    } else if (selection === "clock-out") {
-      res.redirect(`/clock-out/${pin}`);
-    }
-  } else {
-    res.status(404).send({ message: "invalid pin" });
+  employeeService
+    .checkInEmployee(pin)
+    .then((emp) => {
+      console.log(selection);
+      res.redirect(`/${selection}/${emp.uniqueNum}`);
+    })
+    .catch((err) => {
+      res.status(422).json({ message: err });
+    });
+});
+
+app.get("/clock-in/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const employee = await employeeService.checkInEmployee(id);
+
+    const msg = await employeeService.employeeClockIn(employee._id);
+
+    res.json({ message: msg });
+  } catch (err) {
+    res.status(422).send({ message: err });
   }
 });
 
-app.get("/clock-in/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  console.log(id);
-  const result = employee.find((emp) => parseInt(emp.uniqueNum) === id);
-  console.log(result);
-  if (result) {
-    result.timeRecord.push({
-      date: new Date().toLocaleString(),
-      startTime: new Date().toLocaleString(),
-      endTime: null,
-      totalWorkingHours: 0,
-    });
-    const start = result.timeRecord[result.timeRecord.length - 1].startTime;
-    res.status(200).send({
-      message: `you've been successfully clocked in at  ${start}`,
-    });
-    console.log(result);
-  } else {
-    res.status(404).send({ message: "invalid id" });
-  }
-});
+app.get("/clock-out/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const employee = await employeeService.checkInEmployee(id);
 
-app.get("/clock-out/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const result = employee.find((emp) => parseInt(emp.uniqueNum) === id);
-  if (result) {
-    const timeRecord = result.timeRecord[result.timeRecord.length - 1];
-    if (timeRecord.startTime === null) {
-      res.status(400).send({ message: "you have to clock in first" });
-    } else {
-      timeRecord.endTime = new Date().toLocaleString();
-      const start = new Date(timeRecord.startTime).getHours();
-      console.log(start);
-      const end = new Date(timeRecord.endTime).getHours();
-      timeRecord.totalWorkingHours = end - start;
-      res.status(200).send({
-        message: `you've been successfully clocked out at  ${timeRecord.endTime}. Total working hours: ${timeRecord.totalWorkingHours} hours.`,
-      });
-    }
-    console.log(result);
-  } else {
-    res.status(404).send({ message: "invalid id" });
+    const msg = await employeeService.employeeClockOut(employee._id);
+
+    res.json({ message: msg });
+  } catch (err) {
+    res.status(422).send({ message: err });
   }
 });
 
 app.post("/add-employee", (req, res) => {
-  const newEmployee = {
-    id: employee.length + 1,
-    name: req.body.name,
-    uniqueNum: req.body.uniqueNum,
-    timeRecord: [],
-  };
-  try {
-    employee.push(newEmployee);
-    res.status(201).json(employee);
-  } catch (err) {
-    res.status(400).send({ message: "employee not added" });
-  }
+  employeeService
+    .addEmployee(req.body.name, req.body.uniqueNum)
+    .then((msg) => {
+      res.json({ message: msg });
+    })
+    .catch((err) => {
+      res.status(422).send({ message: err });
+    });
 });
 
-app.listen(PORT, () => {
-  console.log(`API is running at http://localhost:${PORT}`);
+app.get("/employee/:uniqueNum", async (req, res) => {
+  const uniqueNum = req.params.uniqueNum;
+
+  console.log(uniqueNum);
+  await employeeService
+    .checkInEmployee(uniqueNum)
+    .then((emp) => {
+      const startTime = new Date(
+        emp.timeRecord[emp.timeRecord.length - 1].startTime
+      ).toLocaleString();
+      const endTime = new Date(
+        emp.timeRecord[emp.timeRecord.length - 1].endTime
+      ).toLocaleString();
+      res.json({
+        message: `Employee ${
+          emp.employeeName
+        } has clocked in at ${startTime} and clocked out at ${endTime}. Total hours: ${
+          emp.timeRecord[emp.timeRecord.length - 1].totalWorkingHours
+        }`,
+      });
+    })
+    .catch((err) => {
+      res.status(422).send({ message: msg });
+    });
 });
+
+employeeService
+  .connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`API is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+  });
